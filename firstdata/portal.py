698817f3560bcb65fd5144fa5d4bda8525faa9f8
Eric Margules <emargules@bluepay.com>
2018-06-13 14:38:05 -0500

    portal.transaction to property from method

--- firstdata/portal.py
+++ firstdata/portal.py
@@ -3,10 +3,8 @@ import firstdata
 class Portal:
 
 	def __init__(self, params):
-			if isinstance(params, firstdata.Config):
-				self.config = params
-			elif isinstance(params, dict):
-				self.config = firstdata.Config(params)
-
-	def transaction(self):
-		return firstdata.TransactionPortal(self)
+		if isinstance(params, firstdata.Config):
+			self.config = params
+		elif isinstance(params, dict):
+			self.config = firstdata.Config(params)
+		self.transaction = firstdata.TransactionPortal(self)
